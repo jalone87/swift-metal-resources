ff78ab2a6e395693597ccb921fddfc25be436509
Lorenzo Tognalini <lorenzo.togna@gmail.com>
2025-02-15 18:39:47 +0100

    loudness measure and processing

--- HeavyMetal/Sources/AudioVisualizer/SignalProcessing.swift
+++ HeavyMetal/Sources/AudioVisualizer/SignalProcessing.swift
@@ -13,14 +13,42 @@ class SignalProcessing {
     /// Calculate the root mean squared of a signal
     /// https://developer.apple.com/documentation/accelerate/vdsp_rmsqv
     static func rms(data: UnsafeMutablePointer<Float>, frameLength: UInt) -> Float {
+        
+        // values [ -1 , 1 ]
+        
         var value = Float(0)
+        
+        // calculate average loudness
         vDSP_measqv(data, 1, &value, frameLength)
+        // values [ 0 , 1 ]
         
+        // convert to decibel
         var db = convertToDecibel(value)
+        // values [ -160 , -0 ] 0 loudest
         
-        db = 160 + db;
-        let dividor = Float(160/0.3)
-        let adjustedVal = 0.3 + db/dividor
+        //inverse dB to +ve range where 0(silent) -> 160(loudest)
+        db = 160 + db
+        // values [ 0 , 160 ]
+        
+        // bring them to a useful scale between intermediated normalized values
+        // let dividor = Float(160/0.3)
+        // let adjustedVal = 0.3 + db/dividor
+        // values [ 0.3 , 0.6 ]
+        
+        // this way values will be in a very narrow bandwith between 0.53 and 0.58, where audible laudness is. let's use another algorithm, and give more importance to that band
+        
+        //Only take into account range from 120->160, so FSR = 40
+        db = db - 120
+        let dividor = Float(40/0.3)
+        var adjustedVal = 0.3 + db/dividor
+        
+        //cutoff
+        if (adjustedVal < 0.3) {
+            adjustedVal = 0.3
+        } else if (adjustedVal > 0.6) {
+            adjustedVal = 0.6
+        }
+        
         return adjustedVal
     }
     
